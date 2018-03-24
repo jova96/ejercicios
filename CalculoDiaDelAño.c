70eec36f32eb6af347bc48574abc95d683b59e73
joseph james <joseph_james96@hotmail.com>
2018-03-24 14:35:18 -0300

    esta basado en tipos enumerativos , y se redujo a un solo switch

--- CalculoDiaDelAño.c
+++ CalculoDiaDelAño.c
@@ -1,119 +1,61 @@
 #include <stdio.h>
 
-typedef enum { DOMINGO = 0 , LUNES , MARTES , MIERCOLES , JUEVES , VIERNES , SABADO = 6 } dia_t;
-
+enum { DOMINGO = 1 , LUNES = 2 , MARTES = 3, MIERCOLES = 4, JUEVES  = 5,
+			VIERNES = 6 , SABADO = 7 } ;          /* se necesita un orden creciente de saltos de a 1 */
+                                                                              /*que comiencen en DOMINGO*/
 int main (void)
 {
 
-	dia_t primer_dia;
-	dia_t segundo_dia;
-	dia_t tercer_dia;
-	dia_t cuarto_dia;
-	dia_t quinto_dia;
-	dia_t sexto_dia;
-	dia_t septimo_dia;
-	dia_t  dia_actual;
-    int dia_ingresado;
+	short unsigned int primer_dia_año;
+    short int dia_ingresado;
 
-	primer_dia = DOMINGO;
+	primer_dia_año = DOMINGO;
 
 	puts("ingresa un valor");
 	scanf("%i",&dia_ingresado);
 
-	switch(primer_dia)
+   if( !( dia_ingresado =(dia_ingresado % 7) ) ){         /* dia_ingresado pasa a ser la posicion del dia */
+		dia_ingresado  += 7                         						 /* tomando el orden definido en enum*/
+	}                                                                                   /* suma 7 para considerar la posicion 0 como 7*/
+
+	if( (dia_ingresado = (dia_ingresado + primer_dia_año - 1 ) )  >= 7 ){    /*para otro pri.._dia_año*/
+		dia_ingresado -=7                                                 /*dia_ingresado pasa a ser la cantidad de saltos */
+	}                                                                                  /* que se hacen a partir del primer_dia_año*/
+                                                                                        /* si supera 7 vuelve a comenzar desde el */
+                                                                                        /*primer dia de la semana considerado en enum*/
+
+	switch(dia_ingresado)
 	{
 
 		case DOMINGO :
-										primer_dia = DOMINGO ; segundo_dia = LUNES ;
-										tercer_dia = MARTES ; cuarto_dia = MIERCOLES ;
-										quinto_dia = JUEVES ; sexto_dia = VIERNES ;
-										septimo_dia = SABADO ;  break;
+			puts("es domingo") ;
+			break;
 
 		case LUNES :
-										primer_dia = LUNES ; segundo_dia = MARTES ;
-										tercer_dia = MIERCOLES ; cuarto_dia = JUEVES ;
-										quinto_dia = VIERNES ; sexto_dia = SABADO ;
-										septimo_dia = DOMINGO ;  break;
+			 puts("es lunes")  ;
+			break;
 
 		case MARTES :
-										primer_dia = MARTES ; segundo_dia = MIERCOLES ;
-										tercer_dia = JUEVES ; cuarto_dia = VIERNES ;
-										quinto_dia = SABADO ; sexto_dia = DOMINGO ;
-										septimo_dia = LUNES ;  break;
+			puts("es martes") ;
+			break;
 
 		case MIERCOLES :
-										primer_dia = MIERCOLES ; segundo_dia = JUEVES ;
-										tercer_dia = VIERNES ; cuarto_dia = SABADO ;
-										quinto_dia = DOMINGO ; sexto_dia = LUNES ;
-										septimo_dia = MARTES ;  break;
+			puts("es miercoles") ;
+			break;
 
 		case JUEVES :
-										primer_dia = JUEVES ; segundo_dia = VIERNES ;
-										tercer_dia = SABADO ; cuarto_dia = DOMINGO ;
-										quinto_dia = LUNES ; sexto_dia = MARTES ;
-										septimo_dia = MIERCOLES ;  break;
+			puts("es jueves") ;
+			break;
 
 		case VIERNES :
-										primer_dia = VIERNES ; segundo_dia = SABADO ;
-										tercer_dia = DOMINGO ; cuarto_dia = LUNES ;
-										quinto_dia = MARTES ; sexto_dia = MIERCOLES ;
-										septimo_dia = JUEVES ;  break;
+			puts("es viernes") ;
+			break;
 
 		case SABADO :
-										primer_dia = SABADO ; segundo_dia = DOMINGO ;
-										tercer_dia = LUNES ; cuarto_dia = MARTES ;
-										quinto_dia = MIERCOLES ; sexto_dia = JUEVES ;
-										septimo_dia = VIERNES ;  break;
-	}
-
-	switch(dia_ingresado % 7)
-	{
-
-		case primer_dia :
-			dia_actual = DOMINGO ; break;
-
-		case segundo_dia :
-			dia_actual = LUNES ;   break;
-
-		case tercer_dia :
-			dia_actual = MARTES ; break;
-
-		case cuarto_dia :
-			dia_actual = MIERCOLES ; break;
-
-		case quinto_dia :
-			dia_actual = JUEVES ;    break;
-
-		case sexto_dia :
-			dia_actual = VIERNES ;  break;
-
-		case septimo_dia :
-			dia_actual = SABADO ;    break;
-	}
-
-	switch(dia_actual)
-	{
-
-		case DOMINGO :
-			puts("es domingo") ; break;
-
-		case LUNES :
-			 puts("es lunes")  ;   break;
-
-		case MARTES :
-			puts("es martes") ; break;
-
-		case MIERCOLES :
-			puts("es miercoles") ; break;
-
-		case JUEVES :
-			puts("es jueves") ;    break;
-
-		case VIERNES :
-			puts("es viernes") ;  break;
-
-		case SABADO :
-			puts("es sabado") ;    break;
+			puts("es sabado") ;
+			break;
+		case default :
+			puts("algo")
 	}
 
 	return 0;
