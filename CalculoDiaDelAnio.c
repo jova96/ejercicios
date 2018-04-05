f904e65aef3736c24689a5ba8769ca06aa781a70
joseph james <joseph_james96@hotmail.com>
2018-04-04 22:30:38 -0300

    programas terminados (rendimiento auto.c , calculodiaanio.c)

---
+++ CalculoDiaDelAnio.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TOTAL_DIAS_ANIO 365      /*puede cambiarse a 366 año bisiesto*/
+
+#define MSJ_INGRESO_DIA "Ingrese el dia (numero) del año para saber que dia es"
+#define MSJ_DIA_DOMINGO "Es domingo"
+#define MSJ_DIA_LUNES "Es lunes"
+#define MSJ_DIA_MARTES "Es martes"
+#define MSJ_DIA_MIERCOLES "Es miercoles"
+#define MSJ_DIA_JUEVES "Es jueves"
+#define MSJ_DIA_VIERNES " Es viernes"
+#define MSJ_DIA_SABADO "Es sabado"
+
+#define MSJ_ERROR_PREFIJO "ERROR"
+#define MSJ_ERROR_INGRESO_INVALIDO "Debe ingresar un dia del año ( 1,2...366)"
+#define MSJ_ERROR_CAMBIO_INESPERADO "Se produjo un error mientras se ejecutaba el programa"
+#define MSJ_ERROR_INGRESO_CERO_NEGATIVO "Ingreso un numero negativo o cero"
+#define MSJ_ERROR_INGRESO_CANTIDAD_DIAS "Los dias comienzan en 1 y acaban en 365 o 366"
+
+enum { DOMINGO = 1 , LUNES = 2 , MARTES = 3, MIERCOLES = 4, JUEVES  = 5,
+			VIERNES = 6 , SABADO = 7 } ;          /* se necesita un orden creciente de saltos de a 1 */
+                                                                              /*que comiencen en DOMINGO*/
+int main (void)
+{
+
+	short unsigned int primer_dia_anio;
+    short int dia_ingresado;
+     int var_aux;
+
+	primer_dia_anio = DOMINGO;                /* todas las opciones son las de enum*/
+
+	puts(MSJ_INGRESO_DIA);
+	if ( (scanf("%hi",&dia_ingresado) ) != 1 ) {          /* si scanf lee bien la entrada , debe devolver 1*/
+		fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_INVALIDO);
+		return EXIT_FAILURE;
+	}
+	while( ( var_aux = getchar() ) != '\n' && var_aux != EOF){
+	}                                                                                     /*limpia el buffer*/
+
+	if( dia_ingresado <= 0 ){
+		fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_CERO_NEGATIVO);
+		return EXIT_FAILURE;
+	}
+
+ 	if( dia_ingresado > TOTAL_DIAS_ANIO ){
+		fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_CANTIDAD_DIAS);
+		return EXIT_FAILURE;
+	}
+
+   if( !( dia_ingresado =(dia_ingresado % 7) ) ){         /* dia_ingresado pasa a ser la posicion del dia */
+		dia_ingresado  += 7;                         					 /* tomando el orden definido en enum*/
+	}                                                                                   /* suma 7 para considerar la posicion 0 como 7*/
+
+	if( (dia_ingresado = (dia_ingresado + primer_dia_anio - 1 ) )  > 7 ){    /*para otro pri.._dia_año*/
+		dia_ingresado -=7;                                                 /*dia_ingresado pasa a ser la cantidad de saltos */
+	}                                                                                  /* que se hacen a partir del primer_dia_año*/
+                                                                                        /* si supera 7 vuelve a comenzar desde el */
+	switch(dia_ingresado)                                               /*primer dia de la semana considerado en enum*/
+	{
+
+		case DOMINGO :
+			puts(MSJ_DIA_DOMINGO) ;
+			break;
+
+		case LUNES :
+			 puts(MSJ_DIA_LUNES)  ;
+			break;
+
+		case MARTES :
+			puts(MSJ_DIA_MARTES) ;
+			break;
+
+		case MIERCOLES :
+			puts(MSJ_DIA_MIERCOLES) ;
+			break;
+
+		case JUEVES :
+			puts(MSJ_DIA_JUEVES) ;
+			break;
+
+		case VIERNES :
+			puts(MSJ_DIA_VIERNES) ;
+			break;
+
+		case SABADO :
+			puts(MSJ_DIA_SABADO) ;
+			break;
+
+	    default :
+			printf("%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_CAMBIO_INESPERADO);
+	}
+
+	return EXIT_SUCCESS;
+
+}
+
+
---
+++ RendimientoAuto.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SALIR -1
+#define KM_POR_LT 100       /*Para mostrar el resultado del rendimiento en litros/100km */
+
+#define MSJ_INGRESO_KM_RECORRIDOS "Ingrese los kilometros recorridos"
+#define MSJ_OPCION_TERMINAR "-1 para terminar"
+#define MSJ_INGRESO_PRECIO_COMBUSTIBLE_POR_L "Ingrese el precio del cobustible por litro"
+#define MSJ_INGRESO_MONTO_TOTAL_POR_COMPRA "Ingrese el monto total utilizado en esta compra"
+#define MSJ_RENDIMIENTO_PARCIAL "Rendimiento (1/100km) de este tanke"
+#define MSJ_RENDIMIENTO_PROMEDIO "Rendimiento promedio (1/100km)"
+#define MSJ_MEJOR_RENDIMIENTO "Mejor rendimiento (1/100km)"
+#define MSJ_PEOR_RENDIMIENTO "Peor rendimiento (1/100km)"
+#define MSJ_KM_TOTAL_RECORRIDO "Distancia total recorrida (km)"
+#define MSJ_COMBUSTIBLE_TOTAL "Combustible total consumido (l)"
+#define MSJ_COSTO_TOTAL_VEHICULO "Costo total de su vehiculo ($)"
+
+#define MSJ_ERROR_PREFIJO "ERROR"
+#define MSJ_ERROR_INGRESO "Se espera un valor numerico"
+#define MSJ_ERROR_INGRESO_SALIR "Ingreso la opcion salir antes de ingresar algun valor"
+#define MSJ_ERROR_INGRESO_NEGATIVO "Se esperan valores ingresados positivos a excepcion de SALIR"
+#define MSJ_ERROR_INGRESO_CERO "NO debe ingresar cero para esta opcion"
+
+int main(void)
+{
+
+	float km_recorridos_parcial = 0;
+	float km_recorridos_total = 0;
+	float precio_combustible_por_L = 0;
+	float monto_combustible_parcial = 0;
+	float monto_combustible_total = 0;
+	float combustible_total_consumido = 0;
+	float rendimiento_parcial = 0;
+	float rendimiento_promedio = 0;
+	float mejor_rendimiento = 0;
+	float peor_rendimiento = 0;
+	float combustible_parcial_consumido = 0;
+	int var_aux ;
+	int estado = 0;
+	int var_conteo = 0;
+
+	/*se ejecuta una vez por lo menos , esta parte se va repetir a final del while*/
+	printf("%s (%s)\n",MSJ_INGRESO_KM_RECORRIDOS,MSJ_OPCION_TERMINAR);
+	if( ( scanf("%f",&km_recorridos_parcial) ) != 1){
+		fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO);
+		return EXIT_FAILURE;
+	}
+	while( ( var_aux = getchar()) != '\n' && var_aux != EOF){
+	}
+
+	if( km_recorridos_parcial == SALIR ){
+		fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_SALIR);
+		return EXIT_FAILURE;
+	}
+
+	if( km_recorridos_parcial == 0){
+		fprintf(stderr,"%s :%s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_CERO);
+		return EXIT_FAILURE;
+	}
+
+	/*Todos los numeros que se utilizan van a a ser positivos , utilizo negativo para la opcion SALIR*/
+	if( km_recorridos_parcial < 0){
+		fprintf(stderr,"%s :%s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_NEGATIVO);
+		return EXIT_FAILURE;
+	}
+
+	while( estado != SALIR ){
+
+		var_conteo +=1;
+		km_recorridos_total += km_recorridos_parcial;
+
+		puts(MSJ_INGRESO_PRECIO_COMBUSTIBLE_POR_L);
+		if( ( scanf("%f",&precio_combustible_por_L) ) != 1){
+			fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO);
+			return EXIT_FAILURE;
+		}
+		while( ( var_aux = getchar()) != '\n' && var_aux != EOF){
+		}
+
+		/*Todos los numeros que se utilizan van a a ser positivos , utilizo negativo para la opcion SALIR*/
+		if( precio_combustible_por_L < 0){
+			fprintf(stderr,"%s :%s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_NEGATIVO);
+			return EXIT_FAILURE;
+		}
+
+		if( precio_combustible_por_L == 0){
+			fprintf(stderr,"%s :%s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_CERO);
+			return EXIT_FAILURE;
+		}
+
+        puts(MSJ_INGRESO_MONTO_TOTAL_POR_COMPRA);
+		if( ( scanf("%f",&monto_combustible_parcial) ) != 1){
+			fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO);
+			return EXIT_FAILURE;
+		}
+		while( ( var_aux = getchar()) != '\n' && var_aux != EOF){
+		}
+
+		if( monto_combustible_parcial < 0){
+			fprintf(stderr,"%s :%s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_NEGATIVO);
+			return EXIT_FAILURE;
+		}
+
+		monto_combustible_total += monto_combustible_parcial;
+		combustible_parcial_consumido = monto_combustible_parcial / precio_combustible_por_L;
+		combustible_total_consumido += combustible_parcial_consumido;
+		rendimiento_parcial = ((combustible_parcial_consumido)*KM_POR_LT ) / km_recorridos_parcial;
+		rendimiento_promedio += rendimiento_parcial;
+
+
+		printf("%s : %.2f\n\n",MSJ_RENDIMIENTO_PARCIAL, rendimiento_parcial);
+
+		if(rendimiento_parcial >= mejor_rendimiento ){
+			mejor_rendimiento = rendimiento_parcial;
+		}
+		if(peor_rendimiento == 0){
+			peor_rendimiento = rendimiento_parcial;
+		}
+		else if(peor_rendimiento > rendimiento_parcial){
+			peor_rendimiento = rendimiento_parcial;
+		}
+
+		printf("%s (%s)\n",MSJ_INGRESO_KM_RECORRIDOS,MSJ_OPCION_TERMINAR);
+		if( ( scanf("%f",&km_recorridos_parcial) ) != 1){
+			fprintf(stderr,"%s: %s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO);
+			return EXIT_FAILURE;
+		}
+		while( ( var_aux = getchar()) != '\n' && var_aux != EOF){
+		}
+
+		if( km_recorridos_parcial == 0){
+			fprintf(stderr,"%s :%s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_CERO);
+			return EXIT_FAILURE;
+		}
+
+		if( km_recorridos_parcial < 0 && km_recorridos_parcial != SALIR ){
+			fprintf(stderr,"%s :%s\n",MSJ_ERROR_PREFIJO,MSJ_ERROR_INGRESO_NEGATIVO);
+			return EXIT_FAILURE;
+		}
+
+		if( km_recorridos_parcial == SALIR ){
+			estado = SALIR;
+		}
+
+	}
+
+	printf("%s >> %.2f\n",MSJ_RENDIMIENTO_PROMEDIO,rendimiento_promedio / var_conteo);
+	printf("%s >> %.2f\n",MSJ_MEJOR_RENDIMIENTO,mejor_rendimiento);
+	printf("%s >> %.2f\n",MSJ_PEOR_RENDIMIENTO,peor_rendimiento);
+	printf("%s >> %.2f\n",MSJ_KM_TOTAL_RECORRIDO,km_recorridos_total);
+	printf("%s >> %.2f\n",MSJ_COMBUSTIBLE_TOTAL,combustible_total_consumido);
+	printf("%s >> %.2f\n",MSJ_COSTO_TOTAL_VEHICULO,monto_combustible_total);
+
+	return  EXIT_SUCCESS;
+
+}
